f48a0efefa6202682ff896dd46cc76b9bc4e232b
ilyav <ilyav@ILIA>
2025-11-17 23:42:50 +0500

    ComboBox is START

--- WinAPI/resource.h
+++ WinAPI/resource.h
@@ -9,7 +9,7 @@
 #define IDC_EDIT_LOGIN                  1003
 #define IDC_EDIT_PASSWORD               1004
 #define IDC_BUTTON_COPY                 1005
-#define IDC_EDIT1                       1007
+#define IDC_EDIT1                       1008
 
 // Next default values for new objects
 // 
@@ -17,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
