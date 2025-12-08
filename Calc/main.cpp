48025ad322d4d16cd633e5ac871029a75c3e09a6
ilyav <ilyav@ILIA>
2025-12-08 23:11:21 +0500

    Calculations is DONE, need DeepCheck

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -230,11 +230,20 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 		if (LOWORD(wParam) == IDC_BUTTON_POINT)
 		{
+			if (input_operation)
+			{
+				sz_buffer[0] = '0';
+				sz_buffer[1] = '.';
+				sz_buffer[2] = 0;
+				input_operation = FALSE;
+			}
 			if (strchr(sz_buffer, '.') == 0) strcat(sz_buffer, ".");
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			input = TRUE;
 		}
 		if (LOWORD(wParam) == IDC_BUTTON_BSP)
 		{
+			if (!input)break;
 			if (strlen(sz_buffer) == 1)sz_buffer[0] = '0';
 			else sz_buffer[strlen(sz_buffer) - 1] = 0;
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
@@ -266,6 +275,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				else b = atof(sz_buffer);
 				input = FALSE;
 			}
+			if (b == DBL_MIN)break;
 			switch (operation)
 			{
 				case IDC_BUTTON_PLUS:  a += b; break;
