5b9475f47a3845b707261f433673d86afa327255
ilyav <ilyav@ILIA>
2025-12-12 23:04:30 +0500

    Single Window color DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -230,6 +230,19 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		SetSkin(hwnd, "square_blue");
 	}break;
 
+	case WM_CTLCOLOREDIT:
+	{
+		HDC hdc = (HDC)wParam;
+		SetBkMode(hdc, OPAQUE);
+		SetBkColor(hdc, RGB(0, 0, 100));
+		SetTextColor(hdc, RGB(255, 0, 0));
+		HBRUSH hBrush = CreateSolidBrush(RGB(50, 50, 150));
+		SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)hBrush);
+		SendMessage(hwnd, WM_ERASEBKGND, wParam, 0);
+		return (LRESULT)hBrush;
+
+	}break;
+
 	case WM_COMMAND:
 	{
 		static DOUBLE a = DBL_MIN, b = DBL_MIN;
